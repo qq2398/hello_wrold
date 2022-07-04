998bdf5b7c65248b60cc096eafbacbe6b8c4bc7f
yimu <refg2398467215@qq.com>
2022-07-04 17:09:29 +0800

    初始化

---
+++ hello-wrold/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>App根组件</h1>
+    <my-count></my-count>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import MyCount from './components/count.vue';
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    MyCount
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ hello-wrold/src/components/count.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+     <h1>{{count}}</h1>
+     <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MyCount',
+    data() {
+        return {    
+            count: 0,
+        }
+    },
+    methods:{
+        increment() {
+            this.count++
+        },
+    }        
+
+}
+</script>
+
+<style>
+
+</style>
